0302f6d69100c99ed26f979b72617e7a8dbf709b
MHopkins <mhopkinsinc@gmail.com>
2024-11-11 16:21:53 -0500

    Fix output file paths in build.bat for assembler execution

--- build.bat
+++ build.bat
@@ -13,6 +13,6 @@ REM Change to the workspace src directory
 cd /d "%workspaceFolder%\src"
 
 REM Run the command with the workspace folder
-%workspaceFolder%assembler\Assembler.exe /p /m /g /o d- /o s- /o r+ /o l+ /o l. /o ow+ /o op- /o os+ /o oz+ /o omq- /o oaq+ /o osq+ "%workspaceFolder%src\hockey.asm,%workspaceFolder%output\nhl92.bin,%workspaceFolder%output\nhl92.map,%workspaceFolder%output\nhl92.lst" > "%workspaceFolder%output\Build.log" 2>&1
+%workspaceFolder%assembler\Assembler.exe /p /m /g /o d- /o s- /o r+ /o l+ /o l. /o ow+ /o op- /o os+ /o oz+ /o omq- /o oaq+ /o osq+ "%workspaceFolder%src\hockey.asm,%workspaceFolder%output\nhl92.bin,%workspaceFolder%output\nhl92,%workspaceFolder%output\nhl92" > "%workspaceFolder%output\Build.log"
 
 endlocal
